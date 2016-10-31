79d3c1c6df7a77a0b3b12c8b980ec29833b57cfb
Rogier Wessel <rhwessel@xs4all.nl>
2016-10-31 09:02:35 +0100

    Updated atom settings

--- atom.sh
+++ atom.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 apm install linter # Base linter
-apm install linter-ruby
-apm install linter-scss-lint
-apm install linter-coffeelint
 apm install linter-rubocop
-apm install linter-haml
+apm install rubocop-auto-correct
+
 # nicer code
 apm install atom-beautify
-# testing
-# apm install ruby-test
-apm install cucumber
-apm install cucumber-step # open the step definition
+
 # language highlighting
-apm install language-rspec
-apm install language-haml
 apm install language-docker
+
 # other
-apm install minimap # Shows you a tiny preview of the file on the right
+apm install minimap
 apm install toggle-quotes
 apm install trailing-spaces
 apm install project-manager
 apm install split-diff
-# for extra credit
-apm install color-picker
-apm install git-blame
+apm install todo-show
+apm install highlight-selected
+apm install minimap-highlight-selected
