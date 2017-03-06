02f88a3b98613a78e1f97444e2f68fcfd91d052a
Rogier Wessel <rhwessel@xs4all.nl>
2017-03-06 23:30:15 +0100

    Updated atom config

--- atom/atom.sh
+++ atom/atom.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-# echo "setting up atom"
-# echo "---> addons"
-# apm install linter # Base linter
-# apm install linter-rubocop
-# apm install rubocop-auto-correct
-#
-# # nicer code
-# apm install atom-beautify
-# apm install vim-mode-plus
-#
-# # language highlighting
-# apm install language-docker
-#
-# # other
-# apm install toggle-quotes
-# apm install trailing-spaces
-# apm install project-manager
-# apm install split-diff
-# apm install todo-show
-# apm install highlight-selected
-#
-# # sql
-# apm install format-sql
+echo "setting up atom"
+echo "---> addons"
+apm install linter # Base linter
+apm install linter-rubocop
+apm install rubocop-auto-correct
+
+# nicer code
+apm install atom-beautify
+apm install vim-mode-plus
+
+# language highlighting
+apm install language-docker
+
+# other
+apm install toggle-quotes
+apm install trailing-spaces
+apm install project-manager
+apm install split-diff
+apm install highlight-selected
+
+# sql
+apm install format-sql
 
 echo "---> projects.cson"
 rm -f $HOME/.atom/projects.cson
